5540fb901373c66e01685c6148cfd52e5b82b402
macbookair <macbookair@macbookairs-MacBook-Air.local>
2026-01-04 00:41:58 +0600

    correction home page and updat-2e

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,10 @@ import Banner from "@/components/home/Banner";
 import Innovation from "@/components/home/Innovation";
 import Service from "@/components/home/Service";
 import Packed from "@/components/home/Packed";
+import AboutSquadLog from "@/components/home/AboutSquadLog";
+import Portfolio from "@/components/home/Protfolio";
+import Support from "@/components/home/Support";
+import Intregration from "@/components/home/Intregration";
 import React from "react";
 
 const TestPages = () => {
@@ -13,11 +17,32 @@ const TestPages = () => {
       <section>
         <Packed />
       </section>
+
+
       <section >
         <Innovation />
       </section>
+
+
       <section>
         <Service />
+      </section>
+
+
+      <section>
+        <Portfolio />
+      </section>
+
+      <section>
+        <Support />
+      </section>
+
+      <section>
+        <AboutSquadLog />
+      </section>
+
+      <section>
+        <Intregration />
       </section>
 
     </div>
