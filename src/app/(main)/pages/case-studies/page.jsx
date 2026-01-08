78cd56fa0b916c9dae8c662f6226ad64c1e9a19c
Aftab Farhan <aftabfarhan324@gmail.com>
2026-01-09 00:31:53 +0600

    Update Case Studies Page Background

--- src/app/(main)/pages/case-studies/page.jsx
+++ src/app/(main)/pages/case-studies/page.jsx
@@ -1,22 +1,28 @@
-import PortfolioCard from '@/components/case-studies/PortfolioCard';
-import PortfolioFqs from '@/components/case-studies/PortfolioFqs';
-import PortfolioReady from '@/components/case-studies/PortfolioReady';
-import React from 'react';
+import PortfolioCard from "@/components/case-studies/PortfolioCard";
+import PortfolioFqs from "@/components/case-studies/PortfolioFqs";
+import PortfolioReady from "@/components/case-studies/PortfolioReady";
+import React from "react";
+import CosmicBackground from "@/components/Home/CosmicBackground";
 
 const Portfoliopage = () => {
-    return (
-        <div className="min-h-screen bg-black overflow-x-hidden pb-30">
-            <section>
-                <PortfolioCard/>
-            </section>
-            <section>
-                <PortfolioFqs />
-            </section>
-            <section>
-                <PortfolioReady/>
-            </section>
-        </div>
-    );
+  return (
+    <div className="min-h-screen relative overflow-x-hidden pb-30">
+      <div className="fixed inset-0 z-0 pointer-events-none">
+        <CosmicBackground />
+      </div>
+      <div className="relative z-10 mt-10">
+        <section>
+          <PortfolioCard />
+        </section>
+        <section>
+          <PortfolioFqs />
+        </section>
+        <section>
+          <PortfolioReady />
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Portfoliopage;
