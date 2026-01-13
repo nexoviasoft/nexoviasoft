69b4da995b5fa38b91b512034f64b375b0fc00f9
Samsuddoha Sojib <Sojib@MacBook-Air.local>
2026-01-14 00:16:35 +0600

    Revert Integration Showcase (FavoriteTools) implementation

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -14,7 +14,6 @@ import Testimonial from "@/components/Home/Testimonial";
 import CosmicBackground from "@/components/Home/CosmicBackground";
 import DevOpsWorkflow from "@/components/Home/DevOpsWorkflow";
 import PortfolioCard from "@/components/case-studies/PortfolioCard";
-import FavoriteTools from "@/components/Home/FavoriteTools";
 
 const TestPages = () => {
   return (
@@ -69,10 +68,6 @@ const TestPages = () => {
 
         <section>
           <Faq />
-        </section>
-
-        <section>
-          <FavoriteTools />
         </section>
 
        
